95460abe3b92f3c8865b88a1846b878d8a5cf3e1
DafandiFadil <dafadarmawan524@student.uns.ac.id>
2026-04-24 15:25:35 +0700

    tambah biar bisa online

--- app/src/main/java/com/travelwaka/app/network/ApiClient.kt
+++ app/src/main/java/com/travelwaka/app/network/ApiClient.kt
@@ -7,16 +7,20 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
-    private const val BASE_URL = "http://10.0.2.2:8000/api/"
+    private const val BASE_URL = "https://unicycle-portside-ungloved.ngrok-free.dev/api/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
-
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(loggingInterceptor)
+        .addInterceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("ngrok-skip-browser-warning", "true")
+                .build()
+            chain.proceed(request)
+        }
         .build()
-
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .client(okHttpClient)
